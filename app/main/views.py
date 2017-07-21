aaaf26577c32657e89e9182c3d1cc8bf435d1647
qiancheng <juggking@163.com>
2017-07-21 05:53:56 -0400

    0722

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,12 @@ from flask import render_template
 
 from . import main
 
-@main.route('/',methods=['GET','POST'])
+@main.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+@main.route('/home', methods=['GET','POST'])
+def home():
+    return render_template('home.html')
+
 
--- func.py
+++ func.py
@@ -1,13 +1,17 @@
 #coding:UTF-8
 #invalid
 from app import BSConfig
+import socket,struct
 
-
-def BSConfigSentToBS(address,):
+def BSConfigSentToBS(localaddress, remoteaddress, SendingStr):
     s=socket.socket(socket.AF_INET, SOCKET_DGRAM)
     except socket.error, msg:
         print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    s.bind(address)
-
+    s.bind(localaddress)
+    str_temp = ""
+    str_temp += struct.pack('B',SendingStr)
+    
+    while True:
+        s.sendto(str_temp, remoteaddress)
 
 
